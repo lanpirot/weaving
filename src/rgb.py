e9eaa5b9c3d8ca48fe11eedfc49623bf36dbf4e1
Alexander Boll <alexander.boll@aisec.fraunhofer.de>
2018-12-11 17:01:26 +0100

    some new functionality for the colors

--- src/rgb.py
+++ src/rgb.py
@@ -7,8 +7,13 @@ class RGB(numpy.ndarray):
  
     def __str__(self):
         self = self.astype(numpy.int16)
-        return '#' + ''.join("00" if n < 0 else "0"+format(n, 'x') if n < 16 else format(n, 'x') for n in self)
+        return '#' + ''.join("00" if n < 0 else "0"+format(n, 'x') if n < 16 else format(n, 'x') if n < 256 else "FF" for n in self)
 
+    def brighten(self):
+        self[:] = (4*self)/3
+
+    def darken(self):
+        self[:] = (3*self)/4
 
 def complement(color):
     white, color = RGB.from_str("#FFFFFF"), RGB.from_str(color)
@@ -21,3 +26,19 @@ def halfway(a, b):
 def color_minus(a, b):
     c1, c2 = RGB.from_str(a), RGB.from_str(b)
     return str(c1 - c2)
+
+def color_add(a, b):
+    c1, c2 = RGB.from_str(a), RGB.from_str(b)
+    return str(c1 + c2)
+
+def color_add_unfaithful(a, b):
+    '''
+    Color b is unfaithfully added to color a. Every color channel of b is multiplied with 3/4 
+    and 30 is subtracted afterwards. (These are arbitrary values).
+    Let color b = "#FF1200" -> b = [255, 18, 0], then the summand b_unfaithful = [140, -17, -30].
+    '''
+    c1, c2 = RGB.from_str(a), RGB.from_str(b)
+    subtrahend = RGB.from_str("#1E1E1E")
+    c2.darken()
+    c2_unfaithful = c2 - subtrahend
+    return str(c1 + c2_unfaithful)
